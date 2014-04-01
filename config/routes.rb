a53ead4fa74631e90c17da35480fe8367294d590
Administrator <ori@pekelman.com>
2014-04-01 18:24:57 +0200

    all of this was much too uglt right? Away ugliness

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ParisRbCassandra::Application.routes.draw do
   resources :heart_beats
 
   resources :people
-
+  root 'people#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
