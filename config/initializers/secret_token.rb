e1d62eb4c40cd99a0ef8cdb305ccfea2cc2d160f
Administrator <ori@pekelman.com>
2014-04-01 16:59:50 +0200

    Initial Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParisRbCassandra::Application.config.secret_key_base = 'd52b25bcf6b64be3e8f987eca01866c1c90addebe68ce8025cee66b89c709f49cad91bed90697c88a6f21713106dde7f5909c6ab6068e4ab1d7a290f1cdfcd60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ParisRbCassandra::Application.config.session_store :cookie_store, key: '_paris-rb-cassandra_session'
