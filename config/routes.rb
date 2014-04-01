b2d4f2d2b9fc3cc464646e3de0b6dca04e4a1925
Administrator <ori@pekelman.com>
2014-04-01 17:06:01 +0200

    Scaffolded Heartbeats and persons added has_many and belongs_to to models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ParisRbCassandra::Application.routes.draw do
+  resources :heart_beats
+
+  resources :people
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
